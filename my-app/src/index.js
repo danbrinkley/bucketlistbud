70246585b135085857cd00912af8109bacff70b5
Danielle Brinkley <daniellebrinkley.fs@gmail.com>
2021-09-25 06:07:17 -0600

    update index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Nav from "./components/Nav";
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Nav />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
